a2734f24f58da42a61584416a7cbd25512d08901
Daniel Rincón Hdez <danielrinconher@gmail.com>
2024-10-18 14:11:04 -0500

    working on dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,27 @@
 	import '../app.css'
 
 	let { children } = $props()
+
+	let darkMode = $state(true)
+
+	//$effect(() => )
+
+	function handleToggleTheme() {
+		darkMode = !darkMode
+
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark')
+	}
 </script>
 
 <svelte:head>
 	<title>danniscript</title>
 </svelte:head>
+
+<header>
+	<input type="checkbox" id="toggle" onclick={handleToggleTheme} />
+	<label for="toggle"></label>
+</header>
 
 {@render children()}
